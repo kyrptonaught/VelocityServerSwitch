169bd2a3c615769c4f7cb9a9a4ea4e2dfec0319f
kyrptonaught <anthonybuttillo10@gmail.com>
2022-11-02 13:11:59 -0400

    Port to 1.19.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+op 2
 # Velocity Server Switch
 
 The Velocity Powered Proxy includes a command to allow the player to switch between servers, but the command does not actually exist on the servers themselves. This cauces issues when you'd like to run the command through another source like a MCF or a command block.
--- src/main/java/net/kyrptonaught/velocityserverswitch/VelocityServerSwitchMod.java
+++ src/main/java/net/kyrptonaught/velocityserverswitch/VelocityServerSwitchMod.java
@@ -1,40 +1,41 @@
 package net.kyrptonaught.velocityserverswitch;
 
-
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.command.argument.MessageArgumentType;
+import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 
 public class VelocityServerSwitchMod implements ModInitializer {
-    public static Identifier BUNGEECORD_ID = new Identifier("bungeecord", "main");
+    private static final Identifier BUNGEECORD_ID = new Identifier("bungeecord", "main");
 
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register(VelocityServerSwitchMod::register);
     }
 
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, boolean b) {
+    private static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
         dispatcher.register(CommandManager.literal("velocityserverswitch")
-                .requires((source) -> source.hasPermissionLevel(0))
+                .requires((source) -> source.hasPermissionLevel(2))
                 .then(CommandManager.argument("servername", StringArgumentType.word())
                         .executes((commandContext) -> {
                             String servername = StringArgumentType.getString(commandContext, "servername");
-                            ByteBufDataOutput output = new ByteBufDataOutput(new PacketByteBuf(Unpooled.buffer()));
+                            try (ByteBufDataOutput output = new ByteBufDataOutput(new PacketByteBuf(Unpooled.buffer()))) {
 
-                            output.writeUTF("Connect");
-                            output.writeUTF(servername);
-                            ServerPlayNetworking.send(commandContext.getSource().getPlayer(), BUNGEECORD_ID, output.getBuf());
+                                output.writeUTF("Connect");
+                                output.writeUTF(servername);
+                                ServerPlayNetworking.send(commandContext.getSource().getPlayer(), BUNGEECORD_ID, output.getBuf());
+                            } catch (Exception e) {
+                                System.out.println("Failed to send switch packet");
+                                e.printStackTrace();
+                            }
                             return 1;
                         })));
     }
