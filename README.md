da33225bd95bc883e41680ce6c56b5b6da9c3a19
gadget78 <crypto@gadet78.uk>
2023-03-18 22:29:46 +0000

    added permission, velocityserverswitch.command, and velocirtyserverswitch.<servername>. added message to player when they dont have permission and try to enter

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Velocity Server Switch
 
-The Velocity Powered Proxy includes a command to allow the player to switch between servers, but the command does not actually exist on the servers themselves. This cauces issues when you'd like to run the command through another source like a MCF or a command block.
+Minecraft "Velocity" Proxy's includes a command to allow the player to switch between servers `/server <servername>`,
 
-This mod adds a command that does exist on the server and can be used like any other command, but emulate the functionally of the Velocity Proxy server switch command: `/velocityserverswitch <servername>`
+but the command does not actually exist on the servers themselves. This causes issues when you'd like to run the command through another source like a normal ingame command, via another mod, MCF or a command block.
+
+
+
+This mod adds that command to the fabric server and can be used like any other command, but emulates the functionally of the Velocity Proxy server switch 
+
+command: `/velocityserverswitch <servername>`
+
+
+## Permissions
+
+permissions node to enable use: `velocityserverswitch.command`
+
+and to enable a server `veloccityserverswitch.<server>`
+for eg `veloccityserverswitch.survival`
+
+if they dont have permission to enter, the user will get a message telling them so.
--- src/main/java/net/kyrptonaught/velocityserverswitch/VelocityServerSwitchMod.java
+++ src/main/java/net/kyrptonaught/velocityserverswitch/VelocityServerSwitchMod.java
@@ -11,31 +11,37 @@ import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
 
 
 public class VelocityServerSwitchMod implements ModInitializer {
     private static final Identifier BUNGEECORD_ID = new Identifier("bungeecord", "main");
-
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register(VelocityServerSwitchMod::register);
     }
-
-    private static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
+    private static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment ) {
         dispatcher.register(CommandManager.literal("velocityserverswitch")
                 .requires(Permissions.require("velocityserverswitch.command"))
                 .then(CommandManager.argument("servername", StringArgumentType.word())
                         .executes((commandContext) -> {
                             String servername = StringArgumentType.getString(commandContext, "servername");
-                            try (ByteBufDataOutput output = new ByteBufDataOutput(new PacketByteBuf(Unpooled.buffer()))) {
-
-                                output.writeUTF("Connect");
-                                output.writeUTF(servername);
-                                ServerPlayNetworking.send(commandContext.getSource().getPlayer(), BUNGEECORD_ID, output.getBuf());
-                            } catch (Exception e) {
-                                System.out.println("Failed to send switch packet");
-                                e.printStackTrace();
+                            String permissionNode = "velocityserverswitch." + servername;
+                            if (Permissions.check(commandContext.getSource().getPlayer(), permissionNode)) {
+                                try (ByteBufDataOutput output = new ByteBufDataOutput(new PacketByteBuf(Unpooled.buffer()))) {
+                                    output.writeUTF("Connect");
+                                    output.writeUTF(servername);
+                                    ServerPlayNetworking.send(commandContext.getSource().getPlayer(), BUNGEECORD_ID, output.getBuf());
+                                } catch (Exception e) {
+                                    System.out.println("Failed to send switch packet");
+                                    e.printStackTrace();
+                                    return 1;
+                                }
+                            } else {
+                                // Player does not have permission to switch to this server
+                                commandContext.getSource().sendFeedback(Text.translatable("commands.velocityserverswitch.no_permission", servername), false);
                             }
                             return 1;
                         })));
